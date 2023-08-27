ddb2f8fbdad61df01580ac6c8a4eaf0703752b8d
Gina <gmroscioli@gmail.com>
2023-08-27 15:47:51 -0400

    gh pages 404 stuff

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,18 @@ import "./index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 
+// check if path is appended as query args (from Github pages 404 redirect)
+((loc) => {
+  if (loc.search[1] === "/") {
+    // if so, put back into the path before we render below
+    window.history.replaceState(
+      null,
+      null,
+      loc.pathname.slice(0, -1) + loc.search.slice(1)
+    );
+  }
+})(window.location);
+
 function Router() {
   return (
     <BrowserRouter basename="/have-a-ball">
